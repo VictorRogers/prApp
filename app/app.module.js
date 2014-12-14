710855b54731909c7ed0b6adc25ff28d1fa8d3e9
Victor Rogers <victor.r.rogers@gmail.com>
2014-12-13 23:38:29 -0600

    Updates

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,36 @@
-var prApp = angular.module('prApp', []);
+var prApp = angular.module('prApp', ['ngRoute', 'ui.bootstrap']);
+
+//config
+prApp.config(function($routeProvider) {
+	$routeProvider
+	
+		//Route - Dashboard
+		.when('/', {
+			templateUrl : 'app/components/dashboard/dashboard.html',
+			controller : 'dashboardController'
+		})
+		
+		//Route - Personnel
+		.when('/personnel', {
+			templateUrl : 'app/components/personnel/personnel.html',
+			controller : 'personnelController'
+		})
+		
+		//Route - Security
+		.when('/security', {
+			templateUrl : 'app/components/security/security.html',
+			controller : 'securityController'
+		})
+		
+		//Route - Health
+		.when('/health', {
+			templateUrl : 'app/components/health/health.html',
+			controller : 'healthController'
+		});
+});
+
+//mainController
+prApp.controller('mainController', function($scope) {
+	$scope.message = 'mainController Test';
+});
+
